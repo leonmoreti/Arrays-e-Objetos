3779f03d2398e9c30304914f58aa40696af934d2
leonmoreti <leonmoreti7@gmail.com>
2022-10-18 16:13:12 -0300

    first commit

---
+++ JS/javascript.js
@@ -0,0 +1,11 @@
+var animal = {}
+
+animal.nome = [];
+animal.peso = '25kg';
+animal.idade = 7;
+animal['idade'] = 24;
+
+animal.nome[0] = 'Bojji'
+animal.nome[1] = 'Frajola'
+
+console.log(animal.nome[0]);
